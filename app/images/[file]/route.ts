6076d2b2dccb5b0abc74307cfef09f8139595eaf
Amirul Zharfan Bin Zalid <amirulzharfanbin@cloudflare.com>
2024-07-09 15:56:37 +0800

    Add http status code

--- app/images/[file]/route.ts
+++ app/images/[file]/route.ts
@@ -8,13 +8,13 @@ export async function GET(request: NextRequest) {
     const fileName = searchParams.get("file");
 
     if (!fileName) {
-        return new Response("File not provided");
+        return new Response("File not provided", { status: 400 });
     }
 
     const file = await getRequestContext().env.R2.get(fileName!);
 
     if (!file) {
-        return new Response("File not found");
+        return new Response("File not found", { status: 404 });
     }
 
     return new Response(await file?.blob(), {
